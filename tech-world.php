f945417b31eb9aaa88a3c5f12ed060ee9cb69758
Yahya Bakr <y.bakr1010@gmail.com>
2024-05-05 13:32:08 +0300

    main structure

--- tech-world.php
+++ tech-world.php
@@ -6,3 +6,33 @@ Version: 1.0
 Author: crackwits
 Author URI: https://crackwits.com
 */
+
+// Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+
+
+add_action( 'wp_dashboard_setup', 'add_dashboard_tech_widget' );
+function add_dashboard_tech_widget() {
+    wp_add_dashboard_widget( 'dashboard_tech_widget', 'Tech World', 'display_dashboard_tech_widget' );
+}
+
+function display_dashboard_tech_widget() {
+
+    echo 'hi';
+}
+
+
+function techworld_quotes_admin_menu() {
+    add_menu_page(
+        'TechWorld Quotes',
+        'TechWorld Quotes',
+        'manage_options',
+        'techworld-quotes',
+        'techworld_quotes_admin_page'
+    );
+}
+add_action('admin_menu', 'techworld_quotes_admin_menu');
+
