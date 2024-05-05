1f62e8deb290477bcddae31e03ceb6597875f3d1
Yahya Bakr <y.bakr1010@gmail.com>
2024-05-05 13:13:53 +0300

    first

---
+++ tech-world.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: TechWorld Plugin
+Description: This plugin adds a custom widget to display a "Quote of the Day" on the WordPress dashboard.
+Version: 1.0
+Author: crackwits
+Author URI: https://crackwits.com
+*/
